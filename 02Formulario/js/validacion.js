c6eb0a3ca8db1958e8b8fbd197823f088d5b3d9b
demonassit <jminorg@ipn.mx>
2024-05-27 08:30:43 -0600

    expresiones regulares

--- 02Formulario/js/validacion.js
+++ 02Formulario/js/validacion.js
@@ -39,4 +39,46 @@ function validar(formulario){
         formulario.nombre.focus();
         return false;
     }
+
+
+    var checkOK = "0123456789";
+
+    var checkString = formulario.edad.value;
+
+    var todoesValido = true;
+
+    for(var i = 0; i < checkString.length; i++){
+        //tengo que transformar la cadena a caracteres
+        var charac = checkString.charAt(i);
+        for(var j = 0; j < checkOK.length; j++){
+            if(charac == checkOK.charAt(j)){
+                break;
+            }
+        }
+        if(j == checkOK.length){
+            todoesValido = false;
+            break;
+        }
+    }
+
+    if(!todoesValido){
+        alert("Escribe unicamente numeros en el campo de edad");
+        formulario.edad.focus();
+        return false;
+    }
+
+    //para validar el correo debemos entender que existe una expresion mediante la cual
+    //debo tener un formato de cadena especifica
+    // sss.sss.ss@sss.sss || ss.ss.@ss.ss || ss@ss.ss
+
+    //toda expresion regular tiene un patron
+
+    var txt = formulario.email.value;
+
+    var patron =  /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
+
+    alert("El email " + (patron.test(txt)?" " : " no ") + "valido");
+
+    return patron.test;
+    
 }
